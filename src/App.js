850e56f6130e55d87791a9d0bc62478991707827
geor-dev <110643142+geor-dev@users.noreply.github.com>
2023-03-13 02:53:53 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import img_principal from './assets/img_tela_principal.png';
+import logo_tech from './assets/logo_sem_fundo.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <img className='img-principal' src={img_principal} alt="img_principal" />
+       
+      <div className='menu-principal-navegação'>{/* início menu de navegação */}
+      
+        <div className='menu-1'>
+        <img className='logo-sem-fundo' src={logo_tech} alt="logo" />
+        </div>
+        
+        <div className='menu-2'>
+          <ul>
+            <li><a href="">INICIO</a></li>
+            <li><a href="">CONTATOS</a></li>
+            <li><a href="">SOBRE</a></li>
+
+          </ul>
+
+        </div>
+
+       
+      </div>{/* fim menu de navegação */}
+
+      
     </div>
   );
 }
